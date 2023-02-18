28b31909e8c8996ce9284ed75cd0c9d127000cc4
LAPTOP-891TQN02\iversonshao <whistle327@gmail.com>
2023-02-18 11:08:15 +0800

    issue 2

--- 110swproblem1.py
+++ 110swproblem1.py
@@ -8,8 +8,18 @@ def search(data, left, right, key):
         return search(data, left, mid-1, key)
     else:
         return search(data, mid+1, right, key)
-def f(x):
-    print(search([1,5,9,14,23,26],0,5,x))
+#def f(x):
+    #print(search([1,5,9,14,23,26],0,5,x))    
 
-f(5)
+def f():
+    data = [1,5,9,14,23,26]
+    my_dict={}
+    my_dict[9]= search(data, 0, 5, 9)
+    my_dict[1]= search(data, 0, 5, 1)
+    my_dict[4]= search(data, 0, 5, 14)
+    print(my_dict)
+    print(data[1:5:2])
+#f(5)
 #f(26)
+
+f()
